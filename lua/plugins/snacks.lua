e00009a557869bb55cf73ab4b153e5a1816ceb6c
Jacob <jacob@saal.com>
2026-06-29 22:52:36 +0200

    Add terminal from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,31 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-        lazygit = { enabled = true },
-    },
-    keys = {
-        { "<leader>gg", function() Snacks.lazygit() end,     desc = "Lazygit" },
-        { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit log (cwd)" },
-    },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		lazygit = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>tt",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit log (cwd)",
+		},
+	},
 }
